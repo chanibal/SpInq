99e6264f4822c4d19fc8e4ecd23e9edca5577442
Krzysztof Bociurko <git@chanibal.pl>
2018-02-03 06:55:52 +0100

    Views and guid utility

--- spinq.php
+++ spinq.php
@@ -93,10 +93,10 @@ class Router {
         $actionName = $request['action'];
 
         if(!isset($this->actions[$actionName]))
-            throw new ActionNotFoundException();
+            throw new ActionNotFoundException("No action of name $actionName found");
         $action = $this->actions[$actionName];
 
-        $action($request);
+        return $action($request, $this);
 	}
 
 }
@@ -108,8 +108,8 @@ class Entity {
 	protected $data;
 
 	public function save() {
-		$s = serialize($this->data);
-		file_put_contents($this->path, $s);
+		$serialized = serialize($this->data);
+		file_put_contents($this->path, $serialized);
 	}
 
 	protected function __construct(array $data, $path) {
@@ -126,10 +126,10 @@ class Entity {
 	public static function load($path) {
 		if(!file_exists($path))
 			return null;
-		$s = file_get_contents($path);
-		$d = unserialize($f);
-		$e = new Entity($data, $path);
-		return $e;
+		$serialized = file_get_contents($path);
+		$data = unserialize($serialized);
+		$entity = new Entity($data, $path);
+		return $entity;
 	}
 
 	/**
@@ -140,4 +140,105 @@ class Entity {
 			throw new RuntimeException("Entity $path already exists");
 		return new Entity(array(), $path);
 	}
+}
+
+
+
+function guid() {
+	return sprintf(
+		'%04X%04X-%04X-%04X-%04X-%04X%04X%04X', 
+		mt_rand(0, 0xFFFF),
+		mt_rand(0, 0xFFFF),
+		mt_rand(0, 0xFFFF),
+		mt_rand(0x4000, 0x4FFF),
+		mt_rand(0, 0xFFFF),
+		mt_rand(0, 0xFFFF),
+		mt_rand(0, 0xFFFF),
+		mt_rand(0, 0xFFFF)
+	);
+}
+
+
+abstract class View {
+	public function headers() { return []; }
+	public function httpCode() { return 200; }
+	public abstract function render();
+	public function __toString() { return $this->render(); }
+}
+
+class JSONView extends View {
+	protected $object;
+	public function __construct($object) {
+		$this->object = $object;
+	}
+
+	public function headers() { return [ 'Content-Type' => 'application/json; charset=utf-8' ]; }
+	public function render() { return json_encode($this->object); }
+}
+
+class HTMLView extends View {
+
+	protected $layouts = [];
+	protected $arguments;
+	protected $httpCode;
+
+	public function __construct($template, $arguments, $httpCode = 200) {
+		$this->wrapIn($template);
+		$this->arguments = $arguments;
+		$this->httpCode = $httpCode;
+	}
+
+	public function httpCode() { return $this->httpCode; }
+
+	public function headers() { return [ 'Content-Type' => 'text/html; charset=utf-8' ]; }
+
+	public function render() {
+		$debug=new Debug('HTMLView');
+		extract($this->arguments);
+		while($template = array_pop($this->layouts)) {
+			$debug->info("Rendering template $template");
+			ob_start();
+			try { include $template; }
+			finally { $contents = ob_get_clean(); }
+		}
+		return $contents;
+	}
+
+
+	public function wrapIn($template) {
+		array_push($this->layouts, $template);
+	}
+
+	public function anchor($url, $text) {
+		$urlEscaped = htmlspecialchars($url);
+		$textEscaped = htmlentities($text);
+		return "<a href=\"{$urlEscaped}\">{$textEscaped}</a>";
+	}
+}
+
+/**
+ * @return View
+ */
+function executeCurrentAction(Router $router) {
+	$debug = new Debug(__FUNCTION__);
+
+	if(isset($GLOBALS['argv'])) {
+		$argv = $GLOBALS['argv']; 
+		$debug->dump($argv);
+		if(count($argv) < 2)
+			throw new RuntimeException('CLI syntax <script> <action> [<arugments in json form>]');
+		$args = count($argv > 2) ? json_decode($argv[2], true) : [];
+		var_dump($args);
+		$args['action'] = $argv[1];
+		$debug->dump($args, 'executing from cli');
+	}
+	else {
+		$args=$_GET + $_POST;
+		$debug->dump($args, 'executing from web');
+	}
+
+	$view = $router->execute($args, $router);
+	if($view instanceof View)
+		return $view;
+	throw new RuntimeException('Action did not return view');
 }
